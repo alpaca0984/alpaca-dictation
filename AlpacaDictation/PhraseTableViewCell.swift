b16b186ca2015ed55ddfb81687d35ed9466761a2
Masataka Hirano <Masataka@Masataka-no-MacBook-Pro.local>
2017-08-01 01:09:45 +0900

    Add TableView and TableCell

---
+++ AlpacaDictation/PhraseTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PhraseTableViewCell.swift
+//  AlpacaDictation
+//
+//  Created by Masataka Hirano on 2017/08/01.
+//  Copyright © 2017 Masataka Hirano. All rights reserved.
+//
+
+import UIKit
+
+class PhraseTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var photoImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
